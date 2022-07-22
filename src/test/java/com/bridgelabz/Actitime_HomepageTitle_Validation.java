1aef39fc0c13f2f8f64658662c29969665b7a11e
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-07-22 07:37:34 -0700

    Added : Verifiy the alignment of username and password in Facebook

--- src/test/java/com/bridgelabz/Actitime_HomepageTitle_Validation.java
+++ src/test/java/com/bridgelabz/Actitime_HomepageTitle_Validation.java
@@ -11,6 +11,10 @@ public class Actitime_HomepageTitle_Validation {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://demo.actitime.com/login.do");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		
 		driver.findElement(By.id("username")).sendKeys("admin"); 
 		driver.findElement(By.name("pwd")).sendKeys("manager"); 
 		driver.findElement(By.xpath("//div[.='Login ']")).click(); 
---
+++ src/test/java/com/bridgelabz/Verify_Alignmentof_UNandPWD_InFB.java
@@ -0,0 +1,34 @@
+package com.bridgelabz;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Verify_Alignmentof_UNandPWD_InFB {
+@Test
+	public void alignmentVerificationOfUNandPWD() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		WebElement unTB = driver.findElement(By.id("email")); 
+//		get the y-coordinate of username field 
+		int username_Ycordinate = unTB.getLocation().getY(); 
+		System.out.println(username_Ycordinate); 
+		WebElement pwdTB = driver.findElement(By.name("pass")); 
+//		get the y-coordinate of password field 
+		int password_Ycordinate = pwdTB.getLocation().getY(); 
+		System.out.println(password_Ycordinate); 
+//	    check whether the Y-coordinate of username and  password field are same 
+		if (username_Ycordinate==password_Ycordinate) { 
+			System.out.println("Both username and password fields are displayed in the same row"); 
+		}else{ 
+			System.out.println("username and password fields are NOT aligned in the same row"); 
+		} 
+			 
+	}
+}
